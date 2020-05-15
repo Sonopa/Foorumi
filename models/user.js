1aa32ad97ebffdc7b24d562d879abfd41cb3ca64
Jussi Mertanen <juzax@hotmail.com>
2020-05-15 11:38:02 +0300

    Add email field to user

--- models/user.js
+++ models/user.js
@@ -7,6 +7,7 @@ var UserSchema = new Schema({
     username: {type: String, required: true, min: 3, max: 16, unique: true},
     password: {type: String, required:true, min:8},
     name: {type: String, min: 1, max: 50},
+    email: {type: String, min: 1},
     comments: [{type: Schema.Types.ObjectId, ref: 'Comment'}],
     discussions: [{type: Schema.Types.ObjectId, ref: 'Discussion'}],
     topics: [{type: Schema.Types.ObjectId, ref: 'Topic'}],
